8175f813fb4e725705091aff1b7527e3924dcfcd
als6 <als6@StaigerA.byui.edu>
2015-01-30 09:41:22 -0700

    Added Wagon class

--- src/byui/cit260/roadToSaltLake/model/Actor.java
+++ src/byui/cit260/roadToSaltLake/model/Actor.java
@@ -5,10 +5,86 @@
  */
 package byui.cit260.roadToSaltLake.model;
 
+import java.io.Serializable;
+import java.util.Objects;
+
 /**
  *
- * @author als6
+ * @author Amy Staiger
  */
-public class Actor {
+public class Actor implements Serializable {
+    //class instance variables  
+    private String gender;
+    private String health;
+    private int age;
+    
+    public String Actor() {
+        return null;
+    }
+
+    public Actor() {
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getHealth() {
+        return health;
+    }
+
+    public void setHealth(String health) {
+        this.health = health;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Actor{" + "gender=" + gender + ", health=" + health + ", age=" + age + '}';
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 11 * hash + Objects.hashCode(this.gender);
+        hash = 11 * hash + Objects.hashCode(this.health);
+        hash = 11 * hash + Objects.hashCode(this.age);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Actor other = (Actor) obj;
+        if (!Objects.equals(this.gender, other.gender)) {
+            return false;
+        }
+        if (!Objects.equals(this.health, other.health)) {
+            return false;
+        }
+        if (!Objects.equals(this.age, other.age)) {
+            return false;
+        }
+        return true;
+    }
+
+   
+    
     
 }
