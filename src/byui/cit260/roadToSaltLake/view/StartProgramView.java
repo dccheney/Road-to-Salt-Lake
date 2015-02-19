c7cca584f235fbe72401d8d28d7642e05cff21d8
AmyLucille <AmyLucille@AmyLucille-HP.c.byui.edu>
2015-02-19 09:46:37 -0700

    Merge origin/master
    Conflicts:
    	.gitignore

--- src/byui/cit260/roadToSaltLake/view/StartProgramView.java
+++ src/byui/cit260/roadToSaltLake/view/StartProgramView.java
@@ -5,24 +5,85 @@
  */
 package byui.cit260.roadToSaltLake.view;
 
+import byui.cit260.roadToSaltLake.model.Player;
+import java.util.Scanner;
+
 /**
  *
  * @author David Cheney & Amy Staiger
  */
 public class StartProgramView {
-    public StartProgramView()
-    {
+    public StartProgramView(){
     }
-    public void startProgram()
-    {
-            this.displayBanner();
+    public void startProgram() {
+    
+        //Display the banner screen    
+        this.displayBanner();
+        
+        //prompt the player to enter theri name Retrieve the name of the player
+        String playersName = this.getPlayersName();
+        
+        //Create and save the player object
+        Player player = ProgramControl.createPlayer(playersName);
+        
+        //Display a personalized welcome message
+        
+        
+        //Display the Main Menu
+    
+    
     }
 
     private void displayBanner() {
-        System.out.println("\n\n*******************************************************************");
-        System.out.println("*                                                                     *"
-                       + "\n* Welcome to the Road to the Great Salt Lake.  In this game you will  *"
-                       + "\n*"
-        );
+        System.out.println("\n*******************************************************************************");
+        
+        System.out.println("*                                                                               *"
+                       + "\n* Welcome to the Road to the Great Salt Lake.  In this game you will            *"
+                       + "\n* be taking on the role as one of the leaders in a wagon team in 1847.          *"
+                       + "\n* You and your family will depart with Brigham Young or one of the              *"
+                       + "\n* Apostles sometime in April or May and should reach the destination            *"
+                       + "\n* in July or August.  The Church of Jesus Christ of Latter-day Saints has       *"
+                       + "\n* been persecuted for their beliefs and has decided to cross the plains and     *"
+                       + "\n* mountains to find refuge in the West.  The Saints have already traveled from  *"
+                       + "\n* hardship to hardship from New York to Ohio and are praying for strength       *"
+                       + "\n* as they prepare for the journey to their 'Promised Land.'                     *");
+        
+        System.out.println("*                                                                               *"
+                       + "\n* The game begins in Winter Quarters, Nebraska.  During the game                *"
+                       + "\n* you will need to purchase supplies, hunt for food, deal with random           *"
+                       + "\n* diseases, and cross the plains, rivers and mountains until Brigham            *"
+                       + "\n* Young declares 'This is the Place'.                                           *" );
+        
+         System.out.println("\n******************************************************************************");
+        
     }
+
+    public String getPlayersName() {
+        boolean valid = false; // indicaes if the name has been retrieved
+        String playersName = null;
+        Scanner keyboard = new Scanner(System.in);  //keyboard input stream
+        
+        while(!valid) { // while a valid name has not been retrieved
+            
+            //prompt for teh player's name
+            System.out.println("Enter the player's name below:");
+            
+            
+            // get the name from the keyboard and trim off the blanks
+            playersName = keyboard.nextLine();
+            playersName = playersName.trim();
+            
+            //if the name is invalid (less than two characters in length
+            if (playersName.length() < 2) {
+                System.out.println("Invalid name - the name must not be blank");
+                continue; //and repeat again
+            }
+            break;
+        
+        }
+        return playersName; // return the name
+        
+                
+                
+                }
 }
--- src/roadtosaltlake/RoadToSaltLake.java
+++ src/roadtosaltlake/RoadToSaltLake.java
@@ -5,14 +5,9 @@
  */
 package roadtosaltlake;
 
-import byui.cit260.roadToSaltLake.model.Actor;
-import byui.cit260.roadToSaltLake.model.Game;
-import byui.cit260.roadToSaltLake.model.Location;
-import byui.cit260.roadToSaltLake.model.Map;
-import byui.cit260.roadToSaltLake.model.Player;
-import byui.cit260.roadToSaltLake.model.Resources;
-import byui.cit260.roadToSaltLake.model.Scene;
-import byui.cit260.roadToSaltLake.model.Wagon;
+import byui.cit260.roadToSaltLake.view.StartProgramView;
+
+
 
 /**
  *
@@ -24,86 +19,9 @@ public class RoadToSaltLake {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-         
-        Wagon wagon = new Wagon();
-        
-        wagon.setAxles(4);
-        wagon.setCover(2);
-        wagon.setWheels(4);
-        
-        String wagonInfo = wagon.toString();
-        
-        Player playerOne = new Player();
-        
-        playerOne.setOccupation("Banker");
-        playerOne.setAge(30);
-        playerOne.setGender("Male");
-        playerOne.setHealth("Good");
-        playerOne.setSkill(10);
-        playerOne.setName("Brigham Young");
-        playerOne.setMoney(400);
-        playerOne.setUserPace(40);
-        playerOne.setUserMinutes(360);
-        
-        String playerOneInfo = playerOne.toString();
-        
-        Resources resources = new Resources();
-        
-        resources.setConsumables(5);
-        resources.setClothing(5);
-        resources.setAmmo(10);
-        resources.setExtraAxles(0);
-        resources.setExtraCovers(0);
-        resources.setExtraWheels(0);
-        resources.setOxen(3);
-        
-        String resourcesInfo = resources.toString();
-        
-        Actor actor = new Actor();
-        
-        actor.setName("John Taylor");
-        actor.setGender("Male");
-        actor.setHealth("Good");
-        actor.setAge(35);
-        
-        String actorInfo = actor.toString();
-        
-        Game game = new Game();
-        game.setTotDistance(200);
-        game.setTotTime(10);
-        
-        String gameInfo = game.toString();
-        
-        Map map = new Map();
-        map.setColCount(2);
-        map.setRowCount(2);
-        
-        String mapInfo = map.toString();
-        
-        Location location = new Location();
-        location.setCol(3);
-        location.setRow(3);
-        location.setVisited(2);
-       
-        String locationInfo = location.toString();
-        
-        Scene scene = new Scene();
-        scene.setSickness("none");
-        scene.setTerrain("Plains");
-        scene.setWeather("Hot");
-        scene.setStore("none");
-        
-        String sceneInfo = scene.toString();
-        
-        System.out.println(wagonInfo);
-        System.out.println(playerOneInfo);
-        System.out.println(resourcesInfo);
-        System.out.println(actorInfo);
-        System.out.println(gameInfo);
-        System.out.println(locationInfo);
-        System.out.println(mapInfo);
-        System.out.println(sceneInfo);
-        
+        StartProgramView startProgramView = new StartProgramView ();
+        startProgramView.startProgram();
+   
    }
     
 }
