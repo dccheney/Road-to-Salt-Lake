5fcfa6158a45afdc9a4a16fefb6b9fa68cc4edb5
David <David@David>
2015-02-19 10:02:51 -0700

    commit hunt, store, huntTest, storeTest

---
+++ src/byui/cit260/roadToSaltLake/control/ProgramControl.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit260.roadToSaltLake.control;
+
+import byui.cit260.roadToSaltLake.model.Player;
+
+/**
+ *
+ * @author David Cheney and Amy Staiger
+ */
+public class ProgramControl {
+    public static Player createPlayer(String playersName)
+    {
+        System.out.println("\n*** createPlayer function called ***");
+        return null;
+    }
+}
