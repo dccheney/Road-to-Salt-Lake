ffaad5c304103ed1bfea8b0050a26a4f1c573087
AmyLucille <AmyLucille@AmyLucille-HP.c.byui.edu>
2015-02-20 10:46:37 -0700

    Merge origin/master
    Conflicts:
    	src/byui/cit260/roadToSaltLake/view/MainMenuView.java

---
+++ src/byui/cit260/roadToSaltLake/control/GameControl.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit260.roadToSaltLake.control;
+
+import byui.cit260.roadToSaltLake.model.Player;
+
+/**
+ *
+ * @author AmyLucille
+ */
+public class GameControl {
+
+    public static void createNewGame(Player player) {
+        System.out.println("\n*** createNewGame stub function called***");
+    }
+    
+}
