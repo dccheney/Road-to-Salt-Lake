32450a95f01f999a1d9e182b8571bb580f656c9e
AmyLucille <AmyLucille@AmyLucille-HP>
2015-02-23 22:17:37 -0700

    Merge origin/master
    Conflicts:
    	src/byui/cit260/roadToSaltLake/view/MainMenuView.java

---
+++ src/byui/cit260/roadToSaltLake/view/ChangePaceView.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit260.roadToSaltLake.view;
+
+import static java.lang.System.out;
+
+/**
+ *
+ * @author AmyLucille
+ */
+public class ChangePaceView {
+    
+    public ChangePaceView() {
+    }
+    public void changePace() {
+        //Display the menu 
+        this.displayMenu();
+        
+        //prompt the player to enter their selection
+        
+        
+        //display the option the player chose
+        
+        
+        //move ahead in the game
+                
+        
+        //display to the player their new location
+        
+        
+    }
+
+    public void displayMenu() {
+        System.out.println("\n****************************");
+        
+        System.out.println("*                                           *"
+                + "\n* When travelling along the trail                  *"
+                + "\n* the travellers encounterd many different         *"
+                + "\n* conditions that affected their pace.  Choose     *"
+                + "\n* wisely and take into account how many rations    *"
+                + "\n* you have and the health of your party members.   *");
+        
+    
+    }
+}
+
+        
