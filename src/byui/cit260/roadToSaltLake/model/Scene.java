18295accd14b41dc84526a774ff6f65d77ac628e
dccheney <che09012@byui.edu>
2015-03-24 10:01:48 -0600

    Merge origin/master
    Conflicts:
    	src/byui/cit260/roadToSaltLake/control/GameControl.java
    	src/byui/cit260/roadToSaltLake/model/Game.java

--- src/byui/cit260/roadToSaltLake/model/Scene.java
+++ src/byui/cit260/roadToSaltLake/model/Scene.java
@@ -12,100 +12,32 @@ import java.util.Objects;
  *
  * @author David Cheney and Amy Staiger
  */
-public class Scene implements Serializable{
+public enum Scene implements Serializable{
+    
+    Nauvoo("swamp",Weather.rainGood,true),;
     
     private String terrain;
-    private String weather;
-    private String sickness;
+    private Weather weather;
     private boolean store;
     
-    public Scene () {
-//	this.terrain = 'flat';
-//	this.weather = 'sunny';
-//	this.sickness = 'none';
-	this.store = true;
-}
     
-    private Location[] locations;
-    
-    public Location[] getLocations() {
-        return locations;
+    Scene (String terrain, Weather weather, boolean store) {
+        this.terrain = terrain;
+        this.weather = weather;
+        this.store = store;
     }
 
-    public void setLocations(Location[] locations) {
-        this.locations = locations;
-    }
-    
 
 
     public String getTerrain() {
         return terrain;
     }
 
-    public void setTerrain(String terrain) {
-        this.terrain = terrain;
-    }
-
-    public String getWeather() {
+    public Weather getWeather() {
         return weather;
     }
 
-    public void setWeather(String weather) {
-        this.weather = weather;
-    }
-
-    public String getSickness() {
-        return sickness;
-    }
-
-    public void setSickness(String sickness) {
-        this.sickness = sickness;
-    }
-
-//    public String getStore() {
-//        return store;
-//    }
-//
-//    public void setStore(String store) {
-//        this.store = store;
-//    }
-
-    @Override
-    public int hashCode() {
-        int hash = 3;
-        hash = 79 * hash + Objects.hashCode(this.terrain);
-        hash = 79 * hash + Objects.hashCode(this.weather);
-        hash = 79 * hash + Objects.hashCode(this.sickness);
-        hash = 79 * hash + Objects.hashCode(this.store);
-        return hash;
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (obj == null) {
-            return false;
-        }
-        if (getClass() != obj.getClass()) {
-            return false;
-        }
-        final Scene other = (Scene) obj;
-        if (!Objects.equals(this.terrain, other.terrain)) {
-            return false;
-        }
-        if (!Objects.equals(this.weather, other.weather)) {
-            return false;
-        }
-        if (!Objects.equals(this.sickness, other.sickness)) {
-            return false;
-        }
-        if (!Objects.equals(this.store, other.store)) {
-            return false;
-        }
-        return true;
-    }
-
-    @Override
-    public String toString() {
-        return "Scene{" + "terrain=" + terrain + ", weather=" + weather + ", sickness=" + sickness + ", store=" + store + '}';
+    public boolean getStore(){
+        return store;
     }
 }
